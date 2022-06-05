1709829d4813995d90f70bb5441232062b801a62
anaskanafani <anas.kanafani10@outlook.com>
2022-06-05 23:49:05 +0400

    finished animation

--- src/App.js
+++ src/App.js
@@ -1,24 +1,75 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect } from "react";
+import { Timeline, Tween } from "react-gsap";
+import { Controller, Scene } from "react-scrollmagic";
+import "./App.css";
 
 function App() {
+  useEffect(() => {
+    const html = document.documentElement;
+    const canvas = document.getElementById("canvas");
+    const context = canvas.getContext("2d");
+
+    const currentFrame = (index) =>
+      `https://www.apple.com/105/media/us/airpods-pro/2019/1299e2f5_9206_4470_b28e_08307a42f19b/anim/sequence/large/01-hero-lightpass/${index
+        .toString()
+        .padStart(4, "0")}.jpg`;
+
+    const preloadImages = () => {
+      for (let i = 1; i < frameCount; i++) {
+        const img = new Image();
+        img.src = currentFrame(i);
+      }
+    };
+
+    const frameCount = 147;
+
+    // canvas setting
+    canvas.width = 1158;
+    canvas.height = 770;
+    const image = new Image();
+    image.src = currentFrame(1);
+
+    image.onload = () => {
+      context.drawImage(image, 0, 0);
+    };
+
+    const updateImage = (index) => {
+      image.src = currentFrame(index);
+      context.drawImage(image, 0, 0);
+    };
+
+    window.addEventListener("scroll", () => {
+      const scrollTop = html.scrollTop;
+      const maxScrollTop = html.scrollHeight - window.innerHeight;
+      const scrollFraction = scrollTop / maxScrollTop;
+
+      const frameIndex = Math.min(
+        frameCount - 1,
+        Math.floor(scrollFraction * frameCount)
+      );
+
+      requestAnimationFrame(() => updateImage(frameIndex + 1));
+      preloadImages();
+    });
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Controller>
+        <Scene triggerHook="onLeave" duration={500} pin>
+          <Timeline>
+            <Tween
+              from={{ fontSize: "4rem", opacity: 1, top: "30%" }}
+              to={{ fontSize: "2rem", opacity: 0, top: "25%" }}
+              duration={1}
+            >
+              <h1>AirPods Pro</h1>
+            </Tween>
+          </Timeline>
+        </Scene>
+      </Controller>
+      <canvas id="canvas"></canvas>
+    </>
   );
 }
 
